2540843756b4d68cb91f37adeed52e13d86254c6
Michelle <>
2024-01-31 18:00:38 +0000

    Remove hard-coded title from header.

--- header.php
+++ header.php
@@ -17,7 +17,6 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" crossorigin="anonymous" />
 
-    <title>Pharmacy First</title>
     <?php wp_head(); ?>
 </head>
 <body>
