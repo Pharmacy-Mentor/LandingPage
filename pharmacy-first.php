f99a4cb1e339cefeb17e902b41843ca14803cb8f
Michelle <>
2024-02-02 14:30:58 +0000

    Add ability to select the hover colour for button and button text.

--- pharmacy-first.php
+++ pharmacy-first.php
@@ -19,3 +19,30 @@ function enable_condition_pages() {
     $value = apply_filters('enable_condition_pages', $value);
     return $value;
 }
+
+/*
+ * Handles the background colour for the buttons on hover.
+ */
+function custom_button_background_colour() {
+    $pfButtonColour = get_field('pf_button_hover_colour');
+    if ($pfButtonColour) {
+        echo '<style>
+            .read-more-btn:hover { background: ' . esc_attr($pfButtonColour) . ' !important; }
+            </style>';
+    }
+}
+
+/*
+ * Handles the text colour for the buttons on hover.
+ */
+function custom_button_text_colour() {
+    $pfButtonTextColour = get_field('pf_button_text_hover_colour');
+    if ($pfButtonTextColour) {
+        echo '<style>
+            .read-more-btn:hover { color: ' . esc_attr($pfButtonTextColour) . ' !important; }
+            </style>';
+    }
+}
+
+add_action('wp_head', 'custom_button_background_colour');
+add_action('wp_head', 'custom_button_text_colour');
